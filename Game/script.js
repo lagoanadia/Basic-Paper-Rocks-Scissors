295220eaf31b355ee971008b2ce71823fb9e9fca
Nadia <lagoanadia@gmail.com>
2026-05-08 11:49:05 +0200

    Implement game logic for Rock-Paper-Scissors

--- Game/script.js
+++ Game/script.js
@@ -1,34 +1,76 @@
-//buttons
-let rock = document.getElementById("rock"); 
-let paper = document.getElementById("paper");
-let scissors = document.getElementById("scissors");
 let buttons = document.querySelectorAll("button");
-//image of ?
 let toReveal = document.getElementById("toReveal");
 
 
-let img = ["images/rock.jpg", "images/paper.jpg", "images/scissors.jpg"];
-//check victory / draw 
-function check()
-{
-    if (toReveal.src == buttons.src)
-    {
-        
-    }
-}
+let wins = document.getElementById("wins");
+let draws = document.getElementById("draws");
+let losses = document.getElementById("losses");
 
-//reveal
-function reveal()
-{
-   toReveal.src = img[Math.floor(Math.random()*img.length)];
-}
+let cWins = 0;
+let cDraws = 0;
+let cLosses = 0;
+
+wins.textContent = cWins;
+draws.textContent = cDraws;
+losses.textContent = cLosses;
+
+
+
+//aligning the choices with the corresponding image
+let options = ["rock","paper","scissors"];
+let img = ["images/rock.jpg", "images/paper.jpg", "images/scissors.jpg"];
+
 
 // addEventListener Syntax = element.addEventListener(event, function, useCapture);
+// the eventListener saves the targeted event's id under event.
 for(let i=0; i < buttons.length; i++)
 {
-   buttons[i].addEventListener("click",reveal);
+   buttons[i].addEventListener("click",reveal); 
 }
 
 
+//generate a random choice 
+function reveal(event)
+{
+   toReveal.src = img[Math.floor(Math.random()*img.length)];
+   choices(event);
+}
 
 
+//establish what each player chose
+let computerChoice="";
+function choices(event)
+{
+   let playerChoice = event.currentTarget.id;
+
+   for(let i=0; i<img.length;i++)
+   {
+      //find out what the computer chose by looking for one of the keywords. 
+      if(toReveal.src.includes(img[i]))
+      {
+         computerChoice = options[i];
+      }
+   }
+   console.log(playerChoice);
+   console.log(computerChoice); 
+   check(playerChoice, computerChoice);
+}
+
+
+function check(playerChoice, computerChoice) {
+   if (playerChoice === computerChoice) {
+      cDraws++;
+   } else if (
+      (playerChoice === "rock" && computerChoice === "scissors") ||
+      (playerChoice === "paper" && computerChoice === "rock") ||
+      (playerChoice === "scissors" && computerChoice === "paper")
+   ) {
+      cWins++;
+   } else {
+      cLosses++;
+   }
+   //update interface
+   wins.textContent = cWins;
+   draws.textContent = cDraws;
+   losses.textContent = cLosses;
+}
